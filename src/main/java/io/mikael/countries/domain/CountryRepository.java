d28a645eff952a28931f613ac54b7733f3604299
Mikael Gueck <gumi@iki.fi>
2014-04-21 12:44:14 +0300

    Initial value.

--- src/main/java/io/mikael/countries/domain/CountryRepository.java
+++ src/main/java/io/mikael/countries/domain/CountryRepository.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,7 +19,7 @@ public class CountryRepository {
     @Value("classpath:countries.json")
     private Resource file;
 
-    private List<Country> data;
+    private List<Country> data = Collections.emptyList();
 
     @PostConstruct
     public void parseCountries() {
